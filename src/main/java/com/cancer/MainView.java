fa4cd9e2ca2c0d5f9abc90dc7dd77831d000551c
Gabriel G Ceola <gabrielceola.gc@gmail.com>
2022-08-10 17:59:53 -0300

    Prmeira tela

--- src/main/java/com/cancer/MainView.java
+++ src/main/java/com/cancer/MainView.java
@@ -2,35 +2,27 @@ package com.cancer;
 
 import com.vaadin.flow.component.Key;
 import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.checkbox.Checkbox;
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
 
-@Route("") 
-public class MainView extends VerticalLayout { 
+@Route("")
+public class MainView extends VerticalLayout {
 
-  private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-public MainView() {
-    VerticalLayout todosList = new VerticalLayout(); 
-    TextField taskField = new TextField(); 
-    Button addButton = new Button("Add"); 
-    addButton.addClickListener(click -> { 
-      Checkbox checkbox = new Checkbox(taskField.getValue());
-      todosList.add(checkbox);
-    });
-    addButton.addClickShortcut(Key.ENTER); 
+	public MainView() {
+		VerticalLayout todosList = new VerticalLayout();
+		TextField taskField = new TextField();
+		Button addButton = new Button("Addd");
+		addButton.addClickListener(click -> 
+			addButton.getUI().ifPresent(ui -> 
+			ui.navigate("Login")));
 
-    add( 
-      new H1("Arthur Viado"),
-      todosList,
-      new HorizontalLayout(
-        taskField,
-        addButton
-      )
-    );
-  }
+		addButton.addClickShortcut(Key.ENTER);
+
+		add(new H1("Arthur Viado"), todosList, new HorizontalLayout(taskField, addButton));
+	}
 }
--- src/main/java/com/cancer/controller/LoginController.java
+++ src/main/java/com/cancer/controller/LoginController.java
@@ -1,19 +1,22 @@
 package com.cancer.controller;
 
+import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.PasswordField;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
 
-
-public class LoginController extends VerticalLayout{
-
-	private TextField txtUsuario;
-	private PasswordField txtSenha;
+@Route("Login")
+public class LoginController extends VerticalLayout {
 	
 	public LoginController() {
-   // TODO document why this constructor is empty
- }
+		VerticalLayout layout = new VerticalLayout();
+		TextField txtUsuario = new TextField("usuario", "Nome de Usuario");
+		PasswordField txtSenha = new PasswordField("senha", "Senha");
+		Button btnLogar = new Button("Login");
+		
+		layout.add(txtUsuario, txtSenha, btnLogar);
+	}
 	
 	
 }
